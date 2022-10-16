8c033a1859e38ad47ff7bd30c4584b53a46c86d8
Faruyakashi <faruyakashi@gmail.com>
2022-10-16 21:11:43 +0400

    update 1

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import subprocess
 import datetime
 import sys, os
 import xml.etree.ElementTree as xml
+import time
 
 curDir = os.path.abspath(os.curdir) # директория программы
 screenCurDir = os.path.abspath(os.curdir) + '/screenshots/' # директория папки со скриншотами
@@ -14,14 +15,27 @@ def getFormat():
 		return formats[int(value) - 1]
 
 # функция записи в XML документ
-def addToXML(name, dtime):
-	root = xml.Element(name)
-	screenName = xml.SubElement(root, 'Name')
-	screenName.text = name
-	screenDtime = xml.SubElement(root, 'Datetime')
-	screenDtime.text = dtime
-	tree = xml.ElementTree(root)	
-	tree.write('struct.xml')
+def createXML():
+	files = os.listdir(screenCurDir)
+	tree = xml.parse('struct.xml')
+	root = tree.getroot()
+	datelist = []
+	for child in root:
+		datelist.append(child.find('date').text)
+	for i in range(len(files)):
+	 	name = str(files[i])
+	 	size = str(os.path.getsize(screenCurDir + files[i]))
+	 	date = str(time.ctime(os.path.getctime(screenCurDir + files[i])))
+	 	if date not in datelist:
+	 		elem = xml.Element(name)
+	 		nElem = xml.SubElement(elem, 'name')
+	 		nElem.text = name
+	 		dElem = xml.SubElement(elem, 'date')
+	 		dElem.text = date
+	 		sElem = xml.SubElement(elem, 'size')
+	 		sElem.text = size
+	 		root.append(elem)
+	 		tree.write('struct.xml')
 
 # функция скриншота всей области
 def fastScreen():
@@ -29,7 +43,6 @@ def fastScreen():
 	name = 'screenshot_' + dtime.strftime('%Y-%m-%d_%H.%M.%S')
 	command = 'ffmpeg -f gdigrab -i desktop ' + screenCurDir + name + getFormat()
 	subprocess.call(command)
-	addToXML(name, dtime.strftime('%Y-%m-%d_%H.%M.%S'))
 
 # функция скриншота определённой области
 def additionalScreen():
@@ -42,16 +55,17 @@ def additionalScreen():
 	name = 'screenshot_' + dtime.strftime('%Y-%m-%d_%H.%M.%S')
 	command = 'ffmpeg -f gdigrab -video_size ' + resolution + ' -offset_x '+ beginX + ' -offset_y '+ beginY + ' -i desktop ' + screenCurDir + name + getFormat()
 	subprocess.call(command)
-	addToXML(name, dtime.strftime('%Y-%m-%d_%H.%M.%S'))
 
 # основной цикл программы
 while True:	
-	os.system('cls')																									
-	value = input('1) Сделать скриншот всей области\n2) Сделать скриншот выделенной области\n3) Выйти\n')																
+	#os.system('cls')																									
+	value = input('1) Сделать скриншот всей области\n2) Сделать скриншот выделенной области\n3) Обновить XML\n4) Выйти\n')																
 	if value == '1':
 		fastScreen()
 	elif value == '2':
 		additionalScreen()
 	elif value == '3':
+		createXML()
+	elif value == '4':
 		sys.exit()
 	else: print('не распознанный выбор\n')
